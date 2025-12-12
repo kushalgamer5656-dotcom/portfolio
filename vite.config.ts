fc5ffe43e09cb6f1c53d50e26963c1b637dba520
kushal khanal <kushalgamer5656@gmail.com>
2025-12-12 20:16:52 +0545

    fix: update vite base path to prevent mime type errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: command === "serve" ? "/" : "/portfolio/",
+  };
+  return config;
 });
